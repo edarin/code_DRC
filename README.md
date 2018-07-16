b63282ab5e5ec298b24e968b7cd35f2226dce8d7
edarin <edith.darin@ensae-paristech.fr>
2018-07-16 17:59:26 +0100

    add cluster attributes to census points

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# code_DRC
+# Code for DRC project
+
+## Section 1: how we've proceeded
+
+###### Step 1 Handling SpatialPolygone object
+- drop kinshasa
+- merge three Bandundu's regions
+
+###### Step 2 Dealing with the census
+
+- merge census (residential/non-residential)
+- add cluster's attributes to points 
+  - first spatial merge (80557 points attributed)
+  - second classic df merge (+2136 points attributed)
+
+## Section 2: what is the output
+
+After the two merges, 365 data points remain without any clusters information because of naming issue in the variable `cluster_id` in the census data
+It corresponds to 20 clusters unidentified:
+
+```
+ cluster_id     
+ [1] "0"               "1"               "102"             "11"              "124"             "129"            
+ [7] "167"             "19"              "2"               "243"             "3"               "4"              
+[13] "5"               "528"             "56"              "58"              "6"               "7"              
+[19] "8"               "drc_kwango_0000"
+`````
--- draw_buffer.R
+++ draw_buffer.R
@@ -9,7 +9,7 @@ ipak <- function(pkg){
 }
 
 # usage
-packages <- c("rgdal", "maptools", "reshape2", "dplyr", "sp")
+packages <- c("rgdal", "maptools", "spatialEco", "dplyr", "sp")
 ipak(packages)
 
 
@@ -50,6 +50,7 @@ data <- full_join(census_res@data, census_nonres@data)
 coords <- rbind(census_res@coords, census_nonres@coords)
 census_full <- SpatialPointsDataFrame(coords, data )
 census_full@proj4string <- boundaries@proj4string
+census_full$index = seq(nrow(census_full))
 # Plot
 plot(census_full, col=census_full@data$res_status)# too many points to see both
 census_extract =census_full[sample(1:83058,50000, replace = F),]
@@ -57,4 +58,48 @@ plot(census_extract,pch =20, col=census_extract@data$res_status, cex=0.3)
 plot( boundaries, add=TRUE)
 
 
-#Transer the attribute from the census_clus to census points - Spatial join here -> https://gis.stackexchange.com/questions/137621/join-spatial-point-data-to-polygons-in-r
+#Transer the attribute from the census_clus to census points 
+names_cluster = colnames(census_clust@data)
+
+
+# First step: Spatial merge
+census_full = point.in.poly(census_full, census_clust)
+census_full@data %>%  filter(is.na(mez_id)) %>% summarise(n())
+
+
+# second step: Dplyr merge
+census_clust$cluster_id = census_clust$mez_id
+df=census_full@data %>%  filter(is.na(mez_id)) %>% select(-names_cluster) %>% left_join(census_clust@data)
+census_full@data[is.na(census_full$mez_id),] = df
+census_full@data %>%  filter(is.na(mez_id))  %>% summarise(n())
+census_full@data %>%  filter(is.na(mez_id)) %>% distinct(cluster_id) %>% summarise(n())
+
+census_full@data %>%  filter(is.na(mez_id)) %>% View()
+
+# EXPLORATION SECTION
+
+
+###### Comparing merging methods
+# Understanding the weird cluster_id 
+census_full@data %>%  filter(is.na(mez_id))  %>% summarise(n())
+census_full@data %>% filter(is.na(mez_id))  %>% group_by(cluster_id) %>% summarise(n()) %>% View()
+
+# Dplyr merge
+merge = census_full@data %>% left_join(census_clust@data)
+merge %>%  filter(is.na(mez_id))  %>% summarise(n())
+
+# look similarities of merging method
+idx.mrg.m= merge %>%  filter(!is.na(mez_id))  %>% select(index) %>% unlist(use.names = FALSE)
+idx.spt.m =census_full@data %>%  filter(!is.na(mez_id))  %>% select(index)%>% unlist(use.names = FALSE)
+idx.m.intersect = intersect(idx.mrg.m, idx.spt.m)
+
+all.equal(merge[idx.m.intersect,],census_full@data[idx.m.intersect,]) 
+        # for merged items same result -> comforting
+
+# Idea: using two steps of merge, first spatial than dplyr
+
+census_full@data = census_full@data %>% left_join(census_clust@data,
+                                                  by="cluster_id")
+sum(is.na(census_full$mez_id.y))
+
+
